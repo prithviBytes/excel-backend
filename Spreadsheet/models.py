9255947c8076c72fa83b917cad3960a7e58ae494
prithviBytes <prithvisuvarna56@gmail.com>
2023-03-29 21:16:48 +0530

    add: models

--- Spreadsheet/models.py
+++ Spreadsheet/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
-# Create your models here.
+class Spreadsheet(models.Model):
+    name = models.CharField(max_length=255)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+
+class Sheet(models.Model):
+    spreadsheet = models.ForeignKey(
+        Spreadsheet, on_delete=models.CASCADE, related_name="sheets"
+    )
+    name = models.CharField(max_length=255)
+    row_count = models.IntegerField()
+    column_count = models.IntegerField()
+
+
+class Row(models.Model):
+    sheet = models.ForeignKey(
+        Sheet, on_delete=models.CASCADE, related_name="rows"
+    )
+
+
+class Cell(models.Model):
+    row = models.ForeignKey(
+        Row, on_delete=models.CASCADE, related_name="cells"
+    )
+    column = models.IntegerField()
+    value = models.CharField(max_length=255)
+    formula = models.CharField(max_length=255, blank=True, null=True)
